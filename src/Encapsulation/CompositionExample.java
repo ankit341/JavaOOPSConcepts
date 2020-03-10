89daf7cc78b1717c86538f60e29fa91de837d9d7
ankit.sourav <ankit.sourav@itcinfotech.com>
2020-03-10 09:29:34 +0530

    Added the Composition Exercise

--- src/Encapsulation/CompositionExample.java
+++ src/Encapsulation/CompositionExample.java
@@ -1,25 +1,48 @@
 package Encapsulation;
 
 class Car {
+    private int id;
+    private String model;
+    private String color;
+    public Car(){
 
-    // Complete its implementation
+    }
+
+    public Car(int id, String model, String color){
+        this.id = id;
+        this.color = color;
+        this.model = model;
+    }
 
 }
 
 class Toyota extends Car {
-
-    // Complete its implementation
+    private ToyotaEngine toyotaEngine;
+    public Toyota(ToyotaEngine toyotaEngine){
+        super();
+        this.toyotaEngine = new ToyotaEngine();
+    }
+    public void setStart(){
+        toyotaEngine.start();
+    }
 
 }
 
 class ToyotaEngine {
 
-    // Complete its implementation
+    public void start(){
+        System.out.println("Engine Start");
+    }
+    public void stop(){
+        System.out.println("Engine Stop");
+    }
 
 }
 
 public class CompositionExample {
     public static void main(String[] args) {
-        // Write your code here
+        ToyotaEngine toyotaEngine = null;
+        Toyota toyotaCar = new Toyota(toyotaEngine);
+        toyotaCar.setStart();
     }
 }
