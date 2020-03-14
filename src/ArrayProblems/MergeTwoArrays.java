eee39b4ab1483862892252736a0374630e89706a
ankit.sourav <ankit.sourav@itcinfotech.com>
2020-03-14 16:38:30 +0530

    Added the Composition Exercise

--- src/ArrayProblems/MergeTwoArrays.java
+++ src/ArrayProblems/MergeTwoArrays.java
@@ -6,22 +6,40 @@ public class MergeTwoArrays {
     public static int[] mergeArrays(int[] arr1, int[] arr2)
     {
         int[] resultantArray = new int[arr1.length + arr2.length];
-        for (int i = 0; i < arr1.length; i++){
-            for (int j = 0; j <arr2.length; j++){
-                if (arr1[i] < arr2[j])
-                    resultantArray[i+j] = arr1[i];
-                else
-                    resultantArray[i+j] = arr2[j];
-            }
+        int i = 0 ,j = 0 ,k = 0;
+        while (i < arr1.length && j < arr2.length){
+            if (arr1[i] < arr2[j])
+                resultantArray[k++] = arr1[i++];
+            else
+                resultantArray[k++] = arr2[j++];
+
+        }
+        while (i < arr1.length){
+            resultantArray[k++] = arr1[i++];
+        }
+        while(j < arr2.length){
+            resultantArray[k++] = arr2[j++];
         }
         return resultantArray;
     }
 
     public static void main(String[] args) {
-        int[] array1 = new int[]{1,3,5,7,9};
+        int[] array1 = new int[]{12,32,52,72,92};
         int[] array2 = new int[]{2,4,6,8,10};
         int[] arr = new int[10];
         System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(mergeArrays(array1,array2)));
     }
 }
+
+//One try to solve this question using for loop
+/*        for (int i = 0; i < arr1.length; i++){
+            int minimumNumber = 0;
+            minimumNumber = arr1[i];
+            for (int j = 0; j < arr2.length; j++){
+                if (minimumNumber > arr2[j]){
+                    minimumNumber = arr2[j];
+                }
+            }
+            resultantArray[i] = minimumNumber;
+        }*/
